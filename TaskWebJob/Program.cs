878a872a14977837f624e2ce988af0976359f1d7
Felix Itiel Arino Gila <FelixItiel.Arino@tajamar365.com>
2021-03-12 12:46:05 +0100

    Add project files.

---
+++ TaskWebJob/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TaskWebJob.Data;
+using TaskWebJob.Repositories;
+
+namespace TaskWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string cnn = "Data Source=sqltajamartete.database.windows.net;Initial Catalog=TETEDB;Persist Security Info=True;User ID=adminsql;Password=Admin123";
+            var provider = new ServiceCollection().AddTransient<RepositoryWebJob>().AddDbContext<WebJobContext>(options =>
+            options.UseSqlServer(cnn)).BuildServiceProvider();
+            RepositoryWebJob repo = provider.GetService<RepositoryWebJob>();
+            Console.WriteLine("PULSE UNA TECLA PARA INICIAR");
+            //Console.ReadLine();
+            repo.PopulateDataWebJob();
+            Console.WriteLine("PROCESO TERMINADO. PULSE ENTER PARA FINALIZAR");
+            //Console.ReadLine();
+        }
+    }
+}
